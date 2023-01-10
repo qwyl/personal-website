5c81289519bcf17e15f98c2c4c795cd056f72111
Qi Yang <qiyang2@berkeley.edu>
2023-01-09 17:39:17 -0800

    added hover dropdown menu, beta

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
-import { Typography } from 'antd';
+import { Typography,Dropdown, Menu } from 'antd';
 import './App.css';
-import Nav from './components/Nav.js';
 const {Title, Text, Paragraph, Link} = Typography;
+const items = [
+  {
+  label:"about me",
+  key:"me/main",
+  },
+  {
+    label:"resume",
+    key:"resumex",
+  },
+  {
+  label:"content",
+  key:"contentstuff",
+  },
+  {
+  label:"log",
+  key:"plogblogvlog",
+  },
+  {
+  label:"future/fun",
+  key:"morestuff",
+  },
+];
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <Title level = {1}> My first React website xD </Title>
-      </header>
-      <Nav></Nav>
+        <Dropdown arrow = {true}
+                  placement = {'topRight'}
+                  menu = {{items}}
+        >
+          <div 
+            style={{
+              width: 100,
+              height: 50,
+              backgroundColor: "lightblue",
+            }}
+          >
+            hover 
+          </div>
+        </Dropdown>
     </div>
   );
 }
